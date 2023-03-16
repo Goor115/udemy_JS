0fcbc5df2fe0e5348032f59271c638ef4b9012a3
Igor <lapayigor@gmail.com>
2023-03-16 14:59:05 +0200

    Done_34

--- 31-intervals-timeouts/finish.js
+++ 31-intervals-timeouts/finish.js
@@ -12,3 +12,20 @@
  * "Сообщение номер 4"
  * "Сообщение номер 5"
  */
+
+// setTimeout(() => console.log("Сообщение номер 1"), 2000)
+// setTimeout(() => console.log('Сообщение номер 2'), 4000)
+// setTimeout(() => console.log('Сообщение номер 3'), 6000)
+// setTimeout(() => console.log('Сообщение номер 4'), 8000)
+// setTimeout(() => console.log('Сообщение номер 5'), 10000)
+
+let i = 1
+
+const intervall = setInterval(() => {
+    console.log('Сообщение номер' + i)
+    i += 1
+}, 2000)
+
+setTimeout(() => {
+    clearInterval(intervall)
+}, 11000)
